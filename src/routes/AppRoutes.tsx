4e5dcc986ac493a80c850b992050e0954a17a243
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-01 19:09:59 +0000

    feat: unauthenticated layout

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { routePaths } from "./route-paths";
+import UnauthenticatedLayout from "../layouts/UnauthenticatedLayout";
 
 function AppRoutes() {
 	return (
 		<Routes>
 			{/* Public Routes */}
 			<Route path="/" element={<p className="text-2xl">App</p>} />
-			<Route path={routePaths.login()} element={<div>Login</div>} />
+			<Route element={<UnauthenticatedLayout />}>
+				<Route path={routePaths.login()} element={<div>Login</div>} />
+			</Route>
 		</Routes>
 	);
 }
