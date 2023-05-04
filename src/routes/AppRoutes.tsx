2a97a58d53935d878d9dcec87f6ba6b189d2afc1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-04 07:49:08 +0000

    feat: route splitting

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,22 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routePaths } from "./route-paths";
 import UnauthenticatedLayout from "../layouts/UnauthenticatedLayout";
-import Login from "../features/login/Login";
+// import Login from "../features/login/Login";
+
+const Login = React.lazy(() => import("../features/login/Login"));
 
 function AppRoutes() {
 	return (
-		<Routes>
-			{/* Public Routes */}
-			<Route path="/" element={<p className="text-2xl">App</p>} />
-			<Route element={<UnauthenticatedLayout />}>
-				<Route path={routePaths.login()} element={<Login />} />
-			</Route>
-		</Routes>
+		<Suspense>
+			<Routes>
+				{/* Public Routes */}
+				<Route path="/" element={<p className="text-2xl">App</p>} />
+				<Route element={<UnauthenticatedLayout />}>
+					<Route path={routePaths.login()} element={<Login />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 }
 
