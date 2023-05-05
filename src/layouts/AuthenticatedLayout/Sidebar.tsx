5c7ee8d027c7ee3510e117218a38b773f6e1a71b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-05 15:14:31 +0000

    feat: add link to logo

--- src/layouts/AuthenticatedLayout/Sidebar.tsx
+++ src/layouts/AuthenticatedLayout/Sidebar.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 function Sidebar() {
 	return (
 		<nav className="flex w-40 flex-col items-center bg-theme-background-offset p-4">
-			<Logo />
+			<Link aria-label="link to home page" to="/">
+				<Logo />
+			</Link>
 		</nav>
 	);
 }
