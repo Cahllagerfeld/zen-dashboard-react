2d8fcc272ea1ef1d8c5519429fdb5df0a87a8919
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-05 08:29:22 +0000

    feat: add logo

--- src/layouts/AuthenticatedLayout/Sidebar.tsx
+++ src/layouts/AuthenticatedLayout/Sidebar.tsx
@@ -1,6 +1,10 @@
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
 function Sidebar() {
 	return (
-		<nav className="flex w-40 flex-col items-center bg-theme-background-offset p-4">Navigation</nav>
+		<nav className="flex w-40 flex-col items-center bg-theme-background-offset p-4">
+			<Logo />
+		</nav>
 	);
 }
 
