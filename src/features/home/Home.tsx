41fba23bb8fdc5efa648cd117f3406b71a9880b6
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-07 06:34:10 +0000

    feat: workspace card

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -3,7 +3,17 @@ import { useWorkspaces } from "./workspace-query";
 function Home() {
 	const { data } = useWorkspaces();
 
-	return <pre>{JSON.stringify(data?.items)}</pre>;
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4">
+			{data?.items.map((item) => (
+				<div className="flex w-full flex-col gap-4 rounded-2xl bg-white p-4">
+					<h2 className="text-xl">{item.name}</h2>
+					{item.description && <p>{item.description}</p>}
+					<time>{new Date(item.created).toLocaleDateString()}</time>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default Home;
