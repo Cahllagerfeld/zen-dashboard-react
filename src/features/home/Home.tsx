1b2702238b307b60ef64f50db5b213ed31a6e71a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-11 20:41:56 +0200

    feat: add Loading skeletons (#33)

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -4,6 +4,8 @@ import { useWorkspaces } from "./workspace-query";
 import { routePaths } from "../../routes/route-paths";
 import { useCurrentUser } from "../../data/user/active-user-query";
 import Welcome from "../../components/Welcome";
+import Skeleton from "react-loading-skeleton";
+import WorkspaceSkeletonCard from "./WorkspaceSkeletonCard";
 
 function Home() {
 	const { setActiveWorkspace } = useWorkspaceStore();
@@ -13,24 +15,31 @@ function Home() {
 	return (
 		<div className="flex flex-col gap-8">
 			<div>
-				<Welcome name={currentUser?.full_name || currentUser?.name || ""} />
+				{currentUser ? (
+					<Welcome name={currentUser.full_name || currentUser.name || ""} />
+				) : (
+					<Skeleton className="h-12 w-1/4 rounded-xl" />
+				)}
 				<p className="text-neutral-400">Please select one of the following workspaces</p>
 			</div>
-
-			<div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4">
-				{data?.items.map((item, index) => (
-					<Link
-						key={index}
-						to={routePaths.workspaces.detail(item.name)}
-						onClick={() => setActiveWorkspace(item.name)}
-						className="flex w-full select-text flex-col gap-2 rounded-2xl bg-white from-primary to-primary-light p-4 hover:bg-primary hover:bg-gradient-to-br hover:text-white"
-					>
-						<h2 className="text-xl">{item.name}</h2>
-						{item.description ? <p>{item.description}</p> : <p>No description provided</p>}
-						<time>{new Date(item.created).toLocaleDateString()}</time>
-					</Link>
-				))}
-			</div>
+			{data ? (
+				<div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4">
+					{data?.items.map((item, index) => (
+						<Link
+							key={index}
+							to={routePaths.workspaces.detail(item.name)}
+							onClick={() => setActiveWorkspace(item.name)}
+							className="flex w-full select-text flex-col gap-2 rounded-2xl bg-white from-primary to-primary-light p-4 hover:bg-primary hover:bg-gradient-to-br hover:text-white"
+						>
+							<h2 className="text-xl">{item.name}</h2>
+							{item.description ? <p>{item.description}</p> : <p>No description provided</p>}
+							<time>{new Date(item.created).toLocaleDateString()}</time>
+						</Link>
+					))}
+				</div>
+			) : (
+				<WorkspaceSkeletonCard />
+			)}
 		</div>
 	);
 }
