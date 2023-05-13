a9d9fea523c9c0db4726cc286fe834fbd93fbd36
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-13 06:27:22 +0000

    feat: add logout button

--- src/layouts/AuthenticatedLayout/Topbar.tsx
+++ src/layouts/AuthenticatedLayout/Topbar.tsx
@@ -1,17 +1,21 @@
 import { useCurrentUser } from "../../data/user/active-user-query";
 import PrimaryCirle from "../../components/primary-circle";
 import Skeleton from "react-loading-skeleton";
+import LogoutButton from "../../components/LogoutButton";
 
 function Topbar() {
 	const user = useCurrentUser();
 	return (
 		<aside className="mx-4 mt-4 flex h-20 items-center justify-between rounded-3xl bg-theme-background-offset p-4">
 			<div></div>
-			{user.data ? (
-				<PrimaryCirle text={user.data.name} />
-			) : (
-				<Skeleton height={48} width={48} circle />
-			)}
+			<div className="flex gap-4">
+				<LogoutButton />
+				{user.data ? (
+					<PrimaryCirle text={user.data.name} />
+				) : (
+					<Skeleton height={48} width={48} circle />
+				)}
+			</div>
 		</aside>
 	);
 }
