1b0de370fdbe02f054031074a0ab7021d9e219f8
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-21 11:01:53 +0000

    feat: Stack Component Query

--- src/features/stack-components/Overview/StackComponents.tsx
+++ src/features/stack-components/Overview/StackComponents.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useWorkspaceStore } from "../../../state/stores/workspace-store";
+import { StackComponentQueryParams, useStackComponents } from "../stack-component-query";
+
 function StackComponentsOverview() {
-	return <p>Stack Components</p>;
+	const [params, _] = useState<StackComponentQueryParams>({});
+	const activeWorkspace = useWorkspaceStore((state) => state.activeWorkspace);
+	const { data } = useStackComponents({ workspace: activeWorkspace, params });
+	return (
+		<div>
+			<p>Stack Components</p>
+			<pre className="block whitespace-normal">{JSON.stringify(data)}</pre>
+		</div>
+	);
 }
 
 export default StackComponentsOverview;
--- src/features/stack-components/stack-component-query.ts
+++ src/features/stack-components/stack-component-query.ts
@@ -1,4 +1,9 @@
-type StackComponentQueryKey = {
+import { UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { useTokenStore } from "../../state/stores";
+import { StackComponent } from "../../types/stack-component";
+import { ErrorModel } from "../../types/error";
+
+type StackComponentQuery = {
 	workspace: string;
 	params: StackComponentQueryParams;
 };
@@ -22,6 +27,30 @@ export type StackComponentQueryParams = {
 	user_id?: string;
 };
 
-export function getStackComponentQueryKEy({ workspace, params }: StackComponentQueryKey) {
+export function getStackComponentQueryKey({ workspace, params }: StackComponentQuery) {
 	return ["workspaces", workspace, "runs", params];
+}
+
+export function useStackComponents(
+	{ workspace, params }: StackComponentQuery,
+	options?: Omit<UseQueryOptions<StackComponent, ErrorModel>, "queryKey" | "queryFn">
+) {
+	const token = useTokenStore((state) => state.token);
+	return useQuery<StackComponent, ErrorModel>({
+		queryKey: getStackComponentQueryKey({ workspace, params }),
+		queryFn: async () => {
+			const response = await fetch(
+				`${import.meta.env.VITE_API_BASE_URL}/workspaces/${workspace}/components`,
+				{
+					method: "GET",
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
+				}
+			);
+			if (!response.ok) throw new Error(`Components couldn't be fetched`);
+			return response.json();
+		},
+		...options
+	});
 }
