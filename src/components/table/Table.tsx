17d24c5307a2ad97a960a59fb7f77614c6608b91
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-21 08:33:08 +0000

    refactor: make table reusable

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,101 +1,57 @@
 import {
 	ColumnDef,
-	createColumnHelper,
+	// createColumnHelper,
 	flexRender,
 	getCoreRowModel,
 	useReactTable
 } from "@tanstack/react-table";
-import { User } from "../../types/user";
+// import { User } from "../../types/user";
 
-// type TableProps<TData, TCol> = {
-// 	data: TData[];
-// 	columnDef: ColumnDef<TCol>;
-// };
+type TableProps<T> = {
+	data: T[];
+	columnDef: ColumnDef<T, any>[];
+	hasActions?: boolean;
+};
 
-const data: Partial<User>[] = [
-	{
-		id: "1",
-		name: "John Doe",
-		email: "john@doe.com",
-		active: true,
-		full_name: "John Doe"
-	},
-	{
-		id: "2",
-		name: "Jane Doe",
-		email: "jane@doe.com",
-		active: false,
-		full_name: "Jane Doe"
-	},
-	{
-		id: "3",
-		name: "Tony Stark",
-		email: "tony@stark.com",
-		active: true,
-		full_name: "Tony Stark"
-	}
-];
-
-const columnHelper = createColumnHelper<Partial<User>>();
-
-const columns = [
-	columnHelper.accessor("id", {
-		header: () => "ID",
-		cell: (id) => id.getValue()
-	}),
-	columnHelper.accessor("name", {
-		header: () => "Name",
-		cell: (info) => info.getValue()
-	}),
-	columnHelper.accessor("email", {
-		header: () => "Email",
-		cell: (mail) => mail.getValue()
-	}),
-	columnHelper.accessor("active", {
-		header: () => "Active",
-		cell: (active) => (active.getValue() ? "Yes" : "No")
-	}),
-	columnHelper.accessor("full_name", {
-		header: () => "Full Name",
-		cell: (full_name) => full_name.getValue()
-	})
-];
-
-function Table() {
+function Table<T>({ data, columnDef, hasActions = false }: TableProps<T>) {
 	const table = useReactTable({
 		data,
-		columns,
+		columns: columnDef,
 		getCoreRowModel: getCoreRowModel()
 	});
 
 	return (
-		<table className="w-full border-separate border-spacing-y-4 text-left">
-			<thead>
-				{table.getHeaderGroups().map((headerGroup) => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map((header) => (
-							<th className="px-2" key={header.id}>
-								{flexRender(header.column.columnDef.header, header.getContext())}
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-			<tbody>
-				{table.getRowModel().rows.map((row) => (
-					<tr key={row.id}>
-						{row.getVisibleCells().map((cell) => (
-							<td
-								className="whitespace-nowrap bg-white p-3 first:rounded-l-lg last:rounded-r-lg"
-								key={cell.id}
-							>
-								{flexRender(cell.column.columnDef.cell, cell.getContext())}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<div className="overflow-auto">
+			<table className="w-full table-auto border-separate border-spacing-y-4 text-left">
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th className="px-2" key={header.id}>
+									{flexRender(header.column.columnDef.header, header.getContext())}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td
+									className={`${
+										hasActions ? "last:w-16" : ""
+									} whitespace-nowrap bg-white p-3 first:rounded-l-lg last:rounded-r-lg`}
+									key={cell.id}
+								>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 }
 
