b35271bf69d68e11a3dba3888d80cf00d0ccc508
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-28 20:08:06 +0200

    feat: prepare fab component (#58)

---
+++ src/components/buttons/FAB.tsx
@@ -0,0 +1,33 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { ButtonHTMLAttributes } from "react";
+
+const fabVariants = cva(
+	"rounded-full absolute h-12 w-12 bottom-12 right-12 transition-all duration-150 active:scale-95",
+	{
+		variants: {
+			variant: {
+				primary: "bg-primary text-white hover:bg-primary-dark"
+			}
+		},
+		defaultVariants: {
+			variant: "primary"
+		}
+	}
+);
+
+type ButtonHTMLProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+interface FABProps
+	extends VariantProps<typeof fabVariants>,
+		Omit<ButtonHTMLProps, "children">,
+		Required<Pick<ButtonHTMLProps, "children">> {}
+
+function FAB({ children, variant, ...props }: FABProps) {
+	return (
+		<button className={fabVariants({ variant })} {...props}>
+			{children}
+		</button>
+	);
+}
+
+export default FAB;
