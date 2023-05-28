63b85b2949046223fa734aa9903dcc30709452be
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-28 20:25:46 +0200

    fix: table skeleton (#63)

--- src/components/table/TableSkeleton.tsx
+++ src/components/table/TableSkeleton.tsx
@@ -1,11 +1,22 @@
 import Skeleton from "react-loading-skeleton";
 
-function TableSkeleton() {
+type TableSkeletonProps = {
+	colAmount?: number;
+};
+
+function TableSkeleton({ colAmount = 5 }: TableSkeletonProps) {
 	return (
 		<div className="flex flex-col gap-4">
+			<div className="flex justify-between gap-8 px-4">
+				{[...Array(colAmount).keys()].map((col) => (
+					<Skeleton containerClassName="block w-full" key={col} className="h-6" />
+				))}
+			</div>
 			{[...Array(10).keys()].map((item) => (
-				<div key={item} className="rounded-2xl bg-white p-4">
-					<Skeleton className="h-6" />
+				<div key={item} className="flex justify-between gap-8 rounded-2xl bg-white p-4">
+					{[...Array(colAmount).keys()].map((col) => (
+						<Skeleton containerClassName="block w-full" key={col} className="h-6" />
+					))}
 				</div>
 			))}
 		</div>
