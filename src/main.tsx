0b388b3df904c80d06c6efbfe60a1a8f3293ad54
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-06-03 07:21:48 +0000

    fix: add global handler

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,20 @@ import App from "./App.tsx";
 import "react-loading-skeleton/dist/skeleton.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider, QueryClient, QueryCache } from "@tanstack/react-query";
+import { FetchError } from "./data/fetch-error.ts";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	queryCache: new QueryCache({
+		onError(error) {
+			if (error instanceof FetchError) {
+				if (error.status === 401) {
+					//TODO Handle Error here
+				}
+			}
+		}
+	})
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
