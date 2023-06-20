3a2d8ea8fa7cc1aee3edec34c60347dbc37e9d75
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-06-20 09:41:36 +0000

    fix: make navbar sticky

--- src/layouts/AuthenticatedLayout/Topbar.tsx
+++ src/layouts/AuthenticatedLayout/Topbar.tsx
@@ -6,17 +6,19 @@ import LogoutButton from "../../components/buttons/LogoutButton";
 function Topbar() {
 	const user = useCurrentUser();
 	return (
-		<aside className="mx-4 mt-4 flex h-20 items-center justify-between rounded-3xl bg-theme-background-offset p-4">
-			<div></div>
-			<div className="flex gap-4">
-				<LogoutButton />
-				{user.data ? (
-					<PrimaryCirle text={user.data.name} />
-				) : (
-					<Skeleton height={48} width={48} circle />
-				)}
-			</div>
-		</aside>
+		<div className="sticky top-0 px-4 pt-4">
+			<aside className="flex h-20 items-center justify-between rounded-3xl bg-theme-background-offset p-4">
+				<div></div>
+				<div className="flex gap-4">
+					<LogoutButton />
+					{user.data ? (
+						<PrimaryCirle text={user.data.name} />
+					) : (
+						<Skeleton height={48} width={48} circle />
+					)}
+				</div>
+			</aside>
+		</div>
 	);
 }
 
