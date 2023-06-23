7f5a05db7152cef453a206db8caa86728da1d4e9
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-06-23 08:23:09 +0000

    feat: add tab to url

--- src/features/stack-components/detail/Tabs.tsx
+++ src/features/stack-components/detail/Tabs.tsx
@@ -1,21 +1,48 @@
-import * as Tabs from "@radix-ui/react-tabs";
+import * as RadixTabs from "@radix-ui/react-tabs";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
-function TabsDemo() {
+function Tabs() {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const tab = searchParams.get("tab");
+
+	const defaultTab = "stacks";
+
+	useEffect(() => {
+		const searchValue = searchParams.get("tab");
+		if (!searchValue) {
+			setSearchParams({ tab: defaultTab }, { replace: true });
+		}
+	}, [searchParams, setSearchParams]);
+
+	function handleTabChange(value: string) {
+		setSearchParams((existing) => {
+			const newSearchParams = new URLSearchParams(existing.toString());
+			newSearchParams.set("tab", value);
+			return newSearchParams;
+		});
+	}
+
 	return (
-		<Tabs.Root className="flex w-full flex-col" defaultValue="stacks">
-			<Tabs.List className="flex h-16 w-full shrink-0 items-center rounded-3xl bg-white px-8 text-2xl">
-				<Tabs.Trigger
+		<RadixTabs.Root
+			onValueChange={handleTabChange}
+			className="flex w-full flex-col"
+			defaultValue={tab || defaultTab}
+		>
+			<RadixTabs.List className="flex h-16 w-full shrink-0 items-center rounded-3xl bg-white px-8 text-2xl">
+				<RadixTabs.Trigger
 					className="flex justify-center data-[state=active]:shadow-[inset_0_-1px_0_0,0_1px_0_0] data-[state=active]:shadow-primary"
 					value="stacks"
 				>
 					Stacks
-				</Tabs.Trigger>
-			</Tabs.List>
-			<Tabs.Content className="grow pt-4" value="stacks">
+				</RadixTabs.Trigger>
+			</RadixTabs.List>
+			<RadixTabs.Content className="grow pt-4" value="stacks">
 				Content
-			</Tabs.Content>
-		</Tabs.Root>
+			</RadixTabs.Content>
+		</RadixTabs.Root>
 	);
 }
 
-export default TabsDemo;
+export default Tabs;
