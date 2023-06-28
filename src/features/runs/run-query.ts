a73477964180c32efa84c28d2ce619b8b4706742
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-06-28 20:33:30 +0200

    feat: adjust run types (#102)

--- src/features/runs/run-query.ts
+++ src/features/runs/run-query.ts
@@ -1,34 +1,10 @@
+import { RunQueryParams } from "@/types/runs";
+
 type RunQueryKey = {
 	workspace: string;
 	params: RunQueryParams;
 };
 
-export type RunQueryParams = {
-	sort_by?: string;
-	logical_operator?: "or" | "and";
-	page?: string;
-	size?: string;
-	id?: string;
-	created?: string;
-	updated?: string;
-	scope_workspace?: string;
-	name?: string;
-	orchestrator_run_id?: string;
-	pipeline_id?: string;
-	workspace_id?: string;
-	user_id?: string;
-	stack_id?: string;
-	schedule_id?: string;
-	build_id?: string;
-	deployment_id?: string;
-	code_repository_id?: string;
-	status?: string;
-	start_time?: string;
-	end_time?: string;
-	num_steps?: string;
-	unlisted?: string;
-};
-
-export function getRunQueryKey({ workspace, params }: RunQueryKey) {
+export function getRunKey({ workspace, params }: RunQueryKey) {
 	return ["workspaces", workspace, "runs", params];
 }
