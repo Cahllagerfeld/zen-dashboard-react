6a82f39a3be47082031c1a7b757fb17143da8549
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-07-02 19:24:52 +0000

    feat: overview Card Skeleton

--- src/features/pipelines/detail/Detail.tsx
+++ src/features/pipelines/detail/Detail.tsx
@@ -4,26 +4,33 @@ import { ReactComponent as PipelineIcon } from "@/assets/pipeline.svg";
 import OverviewCard from "./cards/Overview";
 import DagCard from "./cards/Dag";
 import Tabs from "./Tabs";
+import SkeletonOverview from "./cards/OverviewSkeleton";
+import Skeleton from "react-loading-skeleton";
 
 function PipelineDetail() {
 	const { id } = useParams() as { id: string };
 
-	const { data, isError, isLoading } = usePipelineDetail(id);
+	const { data, isError, isLoading, isSuccess } = usePipelineDetail(id);
 
 	if (isError) {
 		return <p>Error</p>;
 	}
-	if (isLoading) return <p>Fetching</p>;
 
 	return (
 		<div className="flex flex-col gap-4 xl:gap-8">
 			<div className="flex items-center gap-2">
 				<PipelineIcon width={32} height={32} strokeWidth={2.2} />
-				<h1 className="text- text-[2rem]">{data.name}</h1>
+				{isLoading && <Skeleton height={32} width={200} />}
+				{isSuccess && <h1 className="text- text-[2rem]">{data.name}</h1>}
 			</div>
 			<div className="grid grid-cols-1 gap-4 xl:grid-cols-2 xl:gap-8">
-				<OverviewCard pipeline={data} />
-				<DagCard spec={data.spec.steps} />
+				{isLoading && <SkeletonOverview />}
+				{isSuccess && (
+					<>
+						<OverviewCard pipeline={data} />
+						<DagCard spec={data.spec.steps} />
+					</>
+				)}
 			</div>
 			<Tabs id={id} />
 		</div>
