3acfd0120c92379081cef28af9e5c5b05ac32b41
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-09-26 08:42:44 +0200

    feat: add popover (#129)

--- src/layouts/AuthenticatedLayout/Topbar.tsx
+++ src/layouts/AuthenticatedLayout/Topbar.tsx
@@ -1,24 +1,25 @@
 import { useCurrentUser } from "@/data/users/active-user-query";
 import PrimaryCirle from "@/components/primary-circle";
-import Skeleton from "react-loading-skeleton";
-import LogoutButton from "@/components/buttons/LogoutButton";
 import Breadcrumb from "@/components/breadcrumb/Breadcrumb";
 import { useBreadcrumbs } from "@/components/breadcrumb/BreadcrumbContext";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/Popover";
+import { User } from "@/types/user";
+import { useWorkspaceStore } from "@/state/stores/workspace-store";
+import LogoutButton from "@/components/buttons/LogoutButton";
+import { ReactComponent as ChevronDown } from "@/assets/chevron-down.svg";
 
 function Topbar() {
 	const { items } = useBreadcrumbs();
-	const user = useCurrentUser();
+	const { data, isLoading, isError } = useCurrentUser();
+
+	if (isError) return null;
+	if (isLoading) return null;
 
 	return (
 		<aside className="flex h-9 items-center justify-between border-b border-theme-border-moderate bg-theme-surface-primary px-4 py-1">
 			<div>{items.length > 0 && <Breadcrumb items={items} />}</div>
-			<div className="flex gap-2">
-				<LogoutButton />
-				{user.data ? (
-					<PrimaryCirle text={user.data.name} />
-				) : (
-					<Skeleton height={48} width={48} circle />
-				)}
+			<div>
+				<TopbarPopover user={data} />
 			</div>
 		</aside>
 	);
@@ -26,7 +27,28 @@ function Topbar() {
 
 export default Topbar;
 
-// [
-// 	{ label: "Home", href: "/" },
-// 	{ label: "Pipelines", href: "/pipelines" }
-// ]
+type TopbarPopverProps = {
+	user: User;
+};
+
+function TopbarPopover({ user }: TopbarPopverProps) {
+	const { name } = user;
+	const activeWorkspace = useWorkspaceStore((state) => state.activeWorkspace);
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<button className="flex items-center gap-2">
+					<div className="text-end text-text-sm">
+						<p className="font-semibold">{name}</p>
+						<p className="text-theme-text-secondary">{activeWorkspace}</p>
+					</div>
+					<PrimaryCirle text={name} />
+					<ChevronDown className="fill-neutral-400" />
+				</button>
+			</PopoverTrigger>
+			<PopoverContent className="bg-theme-surface-primary" align="end">
+				<LogoutButton />
+			</PopoverContent>
+		</Popover>
+	);
+}
